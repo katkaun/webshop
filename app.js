51a33f5ac1f3b3e7625ce429bee90803eae56afe
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-04-06 11:35:44 +0200

    app,js setup

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', {root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', {root: __dirname });
+});
+
+app.use((req, res) => {
+  res.sendFile('./views/404.html', {root: __dirname });
+})
