c71e6b3b19420635dc6541b46009a8cabb107496
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-04-07 12:08:11 +0200

    commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny')); //to display log request 
 
 
 require('dotenv/config');
