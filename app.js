1299492ef632af0a5d7dc70683cd04aaff04e3cb
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-04-07 12:07:28 +0200

    middleware commit

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+//middleware
+app.use(bodyParser.json());
+
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the webshop!");
+});
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+  }
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
 
 app.listen(3000, () => {
+  
   console.log('Server is running on port 3000');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname });
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', {root: __dirname });
-});
-
-app.use((req, res) => {
-  res.sendFile('./views/404.html', {root: __dirname });
 })
