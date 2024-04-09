780f6697f7d8f95ac17fcd5b1236005c1c2a8b25
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-04-09 10:23:28 +0200

    Preparations done :)

--- models/product.js
+++ models/product.js
@@ -7,6 +7,7 @@ const productSchema = mongoose.Schema({
       type: Number,
       required: true,
     }
-  })
-  
-  exports.Product = mongoose.model('Product', productSchema);
+});
+
+// Export the Product model directly
+module.exports = mongoose.model('Product', productSchema);
