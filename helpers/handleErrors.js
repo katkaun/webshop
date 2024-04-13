01b8fbc32932b5e39ab837d468a85542dec0f1ec
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-04-13 10:35:26 +0200

    Users done

--- helpers/handleErrors.js
+++ helpers/handleErrors.js
@@ -1,12 +1,14 @@
-// function errorHandler(error, req, res, next) {
-//   if(error.name === 'UnauthorizedError') {
-//     //authorization error
-//     return res.status(401).json({message: "unauthorized user"})
-//   }
-//   //validation error
-//   if(error.name === 'ValidationError') {
-//     return res.status(401).json({message: error})
-//   }
-//   //server error
-//   return res.status(500).json({message: error})
-// }
+function handleErrors(error, req, res, next) {
+  if(error.name === 'UnauthorizedError') {
+    //authorization error
+    return res.status(401).json({message: "unauthorized user"})
+  }
+  //validation error
+  if(error.name === 'ValidationError') {
+    return res.status(401).json({message: error})
+  }
+  //server error
+  return res.status(500).json({message: error});
+}
+
+module.exports = handleErrors;
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get(`/`, async (req, res) => {                               // .select('
     res.send(userList);
 })
 
-//Get single user
+//Get a single user
 router.get('/:id', async(req, res) => {
     const user = await User.findById(req.params.id).select('-passwordHash');
     if(!user) {
@@ -23,6 +23,7 @@ router.get('/:id', async(req, res) => {
     res.status(200).send(user);
 })
 
+//For admin who wants to remove or add users
 router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
@@ -44,6 +45,7 @@ router.post('/', async (req, res) => {
     res.send(user);
 })
 
+//A single user login
 router.post('/login', async (req, res) => {
     const user = await User.findOne({email: req.body.email})
     const secret = process.env.secret;
@@ -55,7 +57,8 @@ router.post('/login', async (req, res) => {
     if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
         const token = jwt.sign(
             {
-                userId: user.id              //pass the data you want to with the token
+                userId: user.id,              //pass the data you want to with the token
+                isAdmin: user.isAdmin 
             },          
             secret,                           //pass a secret ex password which is used to create your tokens
             {expiresIn: '1d'}                           
@@ -66,5 +69,54 @@ router.post('/login', async (req, res) => {
         res.status(400).send('Password is incorrect')
     }
 });
+
+    //User registration
+    router.post('/register', async (req, res) => {
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            adress: req.body.adress,
+            apartment: req.body.apartment,
+            city: req.body.city,
+            zip: req.body.zip,
+            country: req.body.country,
+        });
+        user = await user.save();
+    
+        if(!user)
+        return res.status(404).send('The user cannot be created')
+    
+        res.send(user);
+    })
+
+    router.delete('/:id', (req, res) => {
+        User.findByIdAndDelete(req.params.id).then(user => {
+            if(user) {
+                return res.status(200).json({success: true, message: 'The user is deleted'})
+            } else {
+                return res.status(404).json({success: false, message: 'user not found'})
+            }
+        }).catch(error => {
+            return res.status(400).json({success: false, error: error})
+        })
+    })
+
+    router.get(`/get/count`, async (req, res) => {
+        try {
+            const userCount = await User.countDocuments();
+    
+            if (userCount === 0) {
+                res.status(404).json({ success: false, message: 'No users found' });
+                return;
+            }
+    
+            res.json({ success: true, count: userCount });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    });
 
 module.exports = router;
